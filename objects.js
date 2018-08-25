0d99ad51ca5f2be46144e0ce152f9374a43f7b5e
Sasha Hart <sashanicolehart@gmail.com>
2018-08-25 04:01:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { Gershwin: 'April in Paris'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-    
+    Object.assign(playlist, {artistName: songTitle});
+    return playlist;
 }
